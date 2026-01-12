c962376509898766ec38604420a966bb146b47b3
Vossbert <timvossmer@gmail.com>
2026-01-12 11:22:15 +0100

    Added comments and code to EventConcatenate.m to make reproducibility easier.

--- event_detection/EventConcatenate.m
+++ event_detection/EventConcatenate.m
@@ -1,15 +1,19 @@
 clc, clear all, close all;
 %%
-% Load the person_feat variable saved .mat file after running the USLEET code to concatenate
+% Load the person_feat variable saved .mat file after running the main.m code to concatenate
 % each person footstep into 1 single variable
 
-load(['P1_P5_STD_KUR_MSE_Q1_person_feat_2.mat'])
+[filename, filepath] = uigetfile('*.mat', 'Select the .mat file containing the person_feat variable');
+if filename == 0
+    error('No file selected. Exiting.');
+end
 
-
+% Load the person_feat variable from the selected file
+load(fullfile(filepath, filename));
 
 footstep_feat = [];
 labels=[];
-
+% THIS NEEDS TO BE ADJUSTED, BASED ON THE DATASET USED
 for i = 1:5
     i
     footstep_feat = [footstep_feat ; person_feat{i}];
@@ -21,4 +25,8 @@ end
 footstep_feat = [footstep_feat, labels];
 
 %%
-% Now save the footstep_feat variable as .mat file
+% Save the footstep_feat variable automatically in the same folder as the input file
+[~, baseName, ~] = fileparts(filename);
+dataname = sprintf('%s_concatenated.mat', baseName);
+save(fullfile(filepath, dataname), 'footstep_feat');
+fprintf('Saved concatenated data to: %s\n', fullfile(filepath, dataname));
